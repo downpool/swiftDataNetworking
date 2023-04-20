f92a048acf4e042e91636fa38697430f38521837
downpool <downpool.choi@gmail.com>
2023-04-20 17:52:51 +0900

    init

---
+++ swiftDataNetworking/swiftDataNetworking/swiftDataNetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftDataNetworkingApp.swift
+//  swiftDataNetworking
+//
+//  Created by 최동현 on 2023/04/20.
+//
+
+import SwiftUI
+
+@main
+struct swiftDataNetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
